346fe02faa5920ba17bd71ffe3db1e2cd4bc7ec0
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-27 14:41:42 +0530

    ADDED: ability to add weekday and weekend rates for each hotel

--- src/com/bridgelabz/Hotel.java
+++ src/com/bridgelabz/Hotel.java
@@ -2,21 +2,23 @@ package com.bridgelabz;
 
 public class Hotel {
     String name;
-    int rates;
-
+    int weekDayRate;
+    int weekEndRate;
     public Hotel() {
     }
 
-    public Hotel(String name, int rates) {
+    public Hotel(String name, int weekDayRate,int weekEndRate) {
         this.name = name;
-        this.rates = rates;
+        this.weekDayRate = weekDayRate;
+        this.weekEndRate = weekEndRate;
     }
 
     @Override
     public String toString() {
         return "Hotel{" +
                 "name='" + name + '\'' +
-                ", rates=" + rates +
+                ", weekDayRate=" + weekDayRate +
+                ", weekEndRate=" + weekEndRate +
                 '}';
     }
 
@@ -28,11 +30,19 @@ public class Hotel {
         this.name = name;
     }
 
-    public int getRates() {
-        return rates;
+    public int getWeekDayRate() {
+        return weekDayRate;
     }
 
-    public void setRates(int rates) {
-        this.rates = rates;
+    public void setWeekDayRate(int weekDayRate) {
+        this.weekDayRate = weekDayRate;
+    }
+
+    public int getWeekEndRate() {
+        return weekEndRate;
+    }
+
+    public void setWeekEndRate(int weekEndRate) {
+        this.weekEndRate = weekEndRate;
     }
 }
--- src/com/bridgelabz/HotelManagement.java
+++ src/com/bridgelabz/HotelManagement.java
@@ -10,28 +10,29 @@ public class HotelManagement {
     public static HotelManagement hotelManagement = new HotelManagement();
     Hotel hotel;
 
-    public void addHotel(String name, int rates) {
+    public void addHotel(String name, int weekDayRate,int weekEndRate) {
         hotel = new Hotel();
         hotel.setName(name);
-        hotel.setRates(rates);
+        hotel.setWeekDayRate(weekDayRate);
+        hotel.setWeekEndRate(weekEndRate);
         hotelList.add(hotel);
     }
 
     public void insertHotels() {
-        hotelManagement.addHotel("sai", 500);
-        hotelManagement.addHotel("savali", 700);
+        hotelManagement.addHotel("sai", 500,700);
+        hotelManagement.addHotel("savali", 700,1000);
     }
 
     public static void main(String[] args) {
         System.out.println("WelCome the Hotel Management");
         hotelManagement.insertHotels();
         System.out.println(hotelList);
-        LocalDate startDate = LocalDate.of(2022,7,22);
-        LocalDate endDate = LocalDate.of(2022,7,23);
-        System.out.println("Cheapest hotel is: \n"+hotelManagement.cheapestHotel());
+        LocalDate startDate = LocalDate.of(2022, 7, 22);
+        LocalDate endDate = LocalDate.of(2022, 7, 23);
+        System.out.println("Cheapest Rate of WeekDay hotel is: \n" + hotelManagement.cheapestHotel());
     }
 
     private Optional cheapestHotel() {
-       return hotelList.stream().min(Comparator.comparing(Hotel::getRates));
+        return hotelList.stream().min(Comparator.comparing(Hotel::getWeekDayRate));
     }
 }
